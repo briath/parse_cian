ec4310edbbb0e757b31721ff1e13dba4cddc4ccd
Maxim Garazovskiy <baka-96@mail.ru>
2025-08-14 14:48:09 +0300

    fixed

--- app/config.py
+++ app/config.py
@@ -2,16 +2,13 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    POSTGRES_USER: str = "postgres"
-    POSTGRES_PASSWORD: str = "KpSs2uR2"
-    POSTGRES_HOST: str = "localhost"
-    POSTGRES_PORT: int = 5432
-    POSTGRES_DB: str = "postgres"
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "KpSs2uR2"
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_NAME: str = "postgres"
     APP_HOST: str = "0.0.0.0"
     APP_PORT: int = 8000
-    PGADMIN_DEFAULT_EMAIL = "admin@example.com"
-    PGADMIN_DEFAULT_PASSWORD = "PgAdminSecurePass456!"
-    PGADMIN_PORT = 5050
 
     class Config:
         env_file = ".env"
